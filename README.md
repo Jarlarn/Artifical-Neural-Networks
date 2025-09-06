11ca92e17b796c5e5d1cb8b234d4c98f7f30c53b
Jarl Dang <jarldang@gmail.com>
2025-09-06 10:53:16 +0200

    Signum func

---
+++ README.md
@@ -0,0 +1,1 @@
+# This folder contains all the homework for the FFR135 / FIM720 Artificial Neural Networks course at Chalmers.
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from scripts.math_handler import MathHandler
+
+
+handler = MathHandler()
+RESULT = handler.signum_func(2)
+print(RESULT)
